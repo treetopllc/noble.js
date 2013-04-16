c91d103fe95d3b0ced36b4838d5b33c797d7cb95
Dominic Barnes <dbarnes@treetopllc.com>
2013-04-16 14:34:54 -0500

    adding expect.js for tests, extracting client id/secret from lib, adding tests for index/login

--- test/test.Client.js
+++ test/test.Client.js
@@ -1,12 +1,63 @@
-var api = require("api");
+var api = require("api"),
+    request = require("visionmedia-superagent"),
+    expect = require("dominicbarnes-expect.js");
 
 describe("Client", function () {
-    var client = api("http://localhost:7000");
+    var client, config;
+
+    before(function (done) {
+        request("api.json", function (err, res) {
+            if (err) return done(err);
+
+            var data = res.body;
+            client = api(data.api_url, data.client_id, data.client_secret);
+            config = data;
+            done();
+        });
+    });
+
+    describe("#request()", function () {
+        before(function () {
+            client.auth = {
+                access_token: "foo",
+                username: "bar"
+            };
+        });
+
+        after(function () {
+            client.auth = null;
+        });
+
+        it("should add access_token to query string", function (done) {
+            expect(client.request()._query[0]).to.equal("access_token=foo");
+            done();
+        });
+    });
+
+    describe("#index()", function () {
+        it("should be a NH API root", function (done) {
+            client.index(function (err, data) {
+                if (err) return done(err);
+
+                expect(data).to.have.property("description", "Noblehour API");
+                done();
+            });
+        });
+    });
 
     describe("#login()", function () {
-        it("should allow you to log in via username/password", function (done) {
-            client.login("qa+test.user.5@noblehour.com", "123456", function () {
-                console.log(arguments);
+        it("should attach the returned auth data to the client object", function (done) {
+            client.login(config.username, config.password, function (err, auth) {
+                if (err) return done(err);
+
+                expect(auth).to.eql(client.auth);
+                done();
+            });
+        });
+
+        it("should fail for bad username / password combo", function (done) {
+            client.login("not", "real", function (err) {
+                expect(err.message).to.be.equal("Bad username or password");
                 done();
             });
         });
