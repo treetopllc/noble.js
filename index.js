fdfa74da7b9a463fd41d90d4954b93a288a4879b
Dominic Barnes <dbarnes@treetopllc.com>
2013-12-10 11:10:32 -0600

    giving a better error message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ try {
     module.exports = require("./lib");
 } catch (e) {
     if (window.console) {
-        console.warn(e.message);
+        console.warn(e.stack);
     }
 
     module.exports = require("./lib-cov");
