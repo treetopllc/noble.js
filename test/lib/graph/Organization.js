e3390156d1614ee17f3ac9d463e97e571c54d638
April Barrett <abarrett@treetopllc.com>
2014-05-07 11:03:12 -0700

    add organization's content

--- test/lib/graph/Organization.js
+++ test/lib/graph/Organization.js
@@ -1,3 +1,24 @@
-describe.skip("lib/graph/Organization.js", function () {
-    // TODO
+var client = require("../../utils").createClient();
+var simpleResponse = require("../../mock").simpleResponse;
+
+describe("lib/graph/Organization.js", function () {
+    describe("Organization", function () {
+        var organization = client.organization("abc")
+
+        describe("#get(callback)", function () {
+            it("should pass a smoke test", function (done) {
+                server.respondWith("/organizations/abc", simpleResponse);
+
+                organization.get(done);
+            });
+        });
+
+        describe("#content(callback)", function () {
+            it("should pass a smoke test", function (done) {
+                server.respondWith("/organizations/abc/content", simpleResponse);
+
+                organization.content(done);
+            });
+        });
+    });
 });
