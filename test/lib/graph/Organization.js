7c053d2008ebfea0a705a7cc8eb3beae6eb7288c
Dominic Barnes <dbarnes@treetopllc.com>
2014-05-07 16:40:21 -0500

    adding ID translation to Organization#content + test

--- test/lib/graph/Organization.js
+++ test/lib/graph/Organization.js
@@ -1,5 +1,7 @@
 var client = require("../../utils").createClient();
-var simpleResponse = require("../../mock").simpleResponse;
+var mock = require("../../mock");
+var simpleResponse = mock.simpleResponse;
+var defaultHeaders = mock.defaultHeaders;
 
 describe("lib/graph/Organization.js", function () {
     describe("Organization", function () {
@@ -18,6 +20,41 @@ describe("lib/graph/Organization.js", function () {
                 server.respondWith("/organizations/abc/content", simpleResponse);
 
                 organization.content(done);
+            });
+
+            it("should translate id fields into name fields", function (done) {
+                server.respondWith("/organizations/abc/content", [
+                    200,
+                    defaultHeaders,
+                    JSON.stringify([
+                        {
+                            content: {
+                                vertex_type_id: 0 // News
+                            },
+                            author: {
+                                vertex_type_id: 7 // User
+                            },
+                            submitter: {
+                                vertex_type_id: 7 // User
+                            },
+                            submission: {
+                                vertex_type_id: 8, // Submission
+                                status: 1 // Approved
+                            }
+                        }
+                    ])
+                ]);
+
+                organization.content(function (err, results) {
+                    if (err) return done(err);
+                    var row = results[0];
+                    expect(row.content.vertex_type).to.equal("News");
+                    expect(row.author.vertex_type).to.equal("User");
+                    expect(row.submitter.vertex_type).to.equal("User");
+                    expect(row.submission.vertex_type).to.equal("Submission");
+                    expect(row.submission.status_name).to.equal("Approved");
+                    done();
+                });
             });
         });
     });
