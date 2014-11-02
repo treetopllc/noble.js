6abb9ec6ef29ad90ad84cfea3357eeee6a60ca91
April Barrett <abarrett@treetopllc.com>
2014-11-02 18:52:23 -0500

    add missing tests

--- test/lib/graph/Event.js
+++ test/lib/graph/Event.js
@@ -1,3 +1,16 @@
-describe.skip("lib/graph/Event.js", function () {
-    // TODO
+var client = require("../../utils").createClient();
+var simpleResponse = require("../../mock").simpleResponse;
+
+describe("lib/graph/Event.js", function () {
+    describe("Events", function () {
+        var event = client.event("abc");
+
+        describe("#get(params, callback)", function () {
+            it("should pass a smoke test", function (done) {
+                server.respondWith("/events/abc", simpleResponse);
+
+                event.get(done);
+            });
+        });
+    });
 });
