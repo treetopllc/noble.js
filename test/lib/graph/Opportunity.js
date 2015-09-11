770e9d8f97dbbcd1582f8290ceee5c129cf2aeec
April Barrett <abarrett@treetopllc.com>
2015-09-11 01:38:26 -0400

    add event and opp users endpoints

--- test/lib/graph/Opportunity.js
+++ test/lib/graph/Opportunity.js
@@ -1,5 +1,9 @@
-var client = require("../../utils").createClient();
-var simpleResponse = require("../../mock").simpleResponse;
+var utils = require("../../utils");
+var mock = require("../../mock");
+var client = utils.createClient();
+var createArray = utils.createArray;
+var simpleResponse = mock.simpleResponse;
+var defaultHeaders = mock.defaultHeaders;
 
 describe("lib/graph/Opportunity.js", function () {
     describe("Opportunities", function () {
@@ -10,6 +14,32 @@ describe("lib/graph/Opportunity.js", function () {
                 server.respondWith("/opportunities/abc", simpleResponse);
 
                 opportunity.get(done);
+            });
+        });
+
+        describe("#users([query], callback)", function () {
+            it("should pass a smoke test", function (done) {
+                server.respondWith("/opportunities/abc/users", simpleResponse);
+
+                opportunity.users(done);
+            });
+
+            it("should pass additional querystring arguments", function (done) {
+                server.respondWith("/opportunities/abc/users?limit=5", [
+                    200,
+                    defaultHeaders,
+                    JSON.stringify(createArray(5, function () {
+                        return {
+                            id: chance.guid()
+                        };
+                    }))
+                ]);
+
+                opportunity.users({ limit: 5 }, function (err, results) {
+                    if (err) return done(err);
+                    expect(results.length).to.equal(5);
+                    done();
+                });
             });
         });
     });
